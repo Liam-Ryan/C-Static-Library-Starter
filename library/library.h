9744833de976158f8e951bc2f88ebbfea7e48696
Liam Ryan <liomry@gmail.com>
2017-09-07 22:11:58 +0100

    Initial commit

---
+++ library/library.h
@@ -0,0 +1,6 @@
+#ifndef C_STATIC_LIB_SEED_LIBRARY_H
+#define C_STATIC_LIB_SEED_LIBRARY_H
+
+void hello(void);
+
+#endif
---
+++ test/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "../library/library.h"
+
+int main() {
+   fprintf(stdout, "Hello, World!");
+}
